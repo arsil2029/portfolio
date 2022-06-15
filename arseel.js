e0a6b15cd1d11b87734f4e6beeedd9195ab79df7
arsil suthat <98098826+arsil2029@users.noreply.github.com>
2022-06-15 14:56:25 +0530

    Add files via upload

---
+++ arseel.js
@@ -0,0 +1,81 @@
+function show() {
+    nav = document.getElementById('navbar');
+    if (nav.style.display == "none") {
+        nav.style.display = "block"
+
+    }
+    else {
+        nav.style.display = "none"
+    }
+
+
+}
+// setTimeout(show,2000) ;
+
+// setTimeout(show=() => {
+//     nav = document.getElementById('navbar');
+//     if (nav.style.display == "none")
+//      {
+//         nav.style.display = "block"
+
+//     }
+//     else{
+//         nav.style.display = "none"
+//     }  
+
+
+// }, 10000);
+
+// show.addEventListener('click'function(){
+
+//     nav = document.getElementById('navbar');
+//     if (nav.style.display == "none")
+//      {
+//         nav.style.display = "block"
+
+//     }
+//     else{
+//         nav.style.display = "none"
+//     } 
+// })
+function test1() {
+
+    document.getElementById('img').src = "img/web-design.jpg"
+    document.getElementById('change1').innerHTML = "Web Design"
+    document.getElementById('change2').innerHTML = "Decorations don’t drive home messages. Content does. Reducing text-based content to a visual design element (the shape of the text) can result in bloated and unrealistic client expectations once real data replaces the dummy content. We allow our design decisions to be dictated by the on-page content and messaging, and often our designers use the actual content to inspire interesting elements that might not have been conceived without it"
+
+
+}
+
+function test2() {
+
+    document.getElementById('img').src = "img/web-development.jpg"
+    document.getElementById('change1').innerHTML = "Web Development"
+    document.getElementById('change2').innerHTML = "Once we have come up with a unique design and have finalized the textures and graphics to be added, the next step is to make it all come together. And that is what we aim to achieve at our web development agency India. Only a professionally designed website can justify the uniqueness of your idea and this is a fact clearly understood by our team."
+
+}
+
+function test3() {
+
+    document.getElementById('img').src = "img/mobile-development.jpg"
+    document.getElementById('change1').innerHTML = "Mobile Development"
+    document.getElementById('change2').innerHTML = "With an increasing importance being given to smart phones, and mobile apps, it has become immensely important to include app development as part of web design services. Our team is known to provide excellent and extraordinary apps that are unique in every way. All the apps developed by our team are based on providing interesting features combined with enhanced functionality. With a mobile app in place, you can increase the reach of your brand and broaden its horizons too. You can have an easy to use professional app created that provides ease of functionality and an amazing appeal."
+
+
+}
+
+function test4() {
+
+    document.getElementById('img').src = "img/seo-optimization.jpg"
+    document.getElementById('change1').innerHTML = "SEO optimization"
+    document.getElementById('change2').innerHTML = "Search engine optimization (SEO) is the process of improving the visibility of a web site in search engines. Consumer puts a lot of trust in search engines to find what they need. Google receives 34,000 searches per second. Those searches involve finding products, reviewing brands, and looking up business locations."
+
+}
+
+function test5() {
+
+    document.getElementById('img').src = "img/social-media.jpg"
+    document.getElementById('change1').innerHTML = "Social Media"
+    document.getElementById('change2').innerHTML = "Social Media, has become an essential tool of marketing an online business. It gives you a platform to interact and inform people about yourself and your brand. The concept of social media basically refers to the task of promoting websites or business through social media platforms.."
+
+}
